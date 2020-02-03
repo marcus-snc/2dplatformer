da5ba6ef74d48b9c5529deeff41aef19cf6d22c4
daupaloffer <marcus@jadena.co.uk>
2020-02-03 18:57:50 +0000

    Version 0.01
    Finish starter level
    Add camera follow, initial player control
    Tweak renderer lighting settings

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public Transform target;
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //public Rigidbody rb;
+    public Vector2 force;
+    private Rigidbody2D rb;
+    public bool grounded = true;
+    // Start is called before the first frame update
+    void Start() {
+        force = new Vector2(1, 0);
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    void FixedUpdate() {
+        if (Input.GetKey("d")) {
+            //Debug.Log(Input.GetAxis("Horizontal"));
+            //rb.AddForce(new Vector2(1,0)*10);
+            rb.AddRelativeForce(Vector2.right * 75);
+        }
+        else if (Input.GetKey("a")) {
+            //Debug.Log(Input.GetAxis("Horizontal"));
+            //rb.AddForce(new Vector2(-1,0)*10);
+            rb.AddRelativeForce(Vector2.left * 75);
+        }
+
+        if (Input.GetButton("Jump")) {
+            if (grounded == true) {
+                rb.AddRelativeForce(Vector2.up * 500);
+            }
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.name == "Tilemap") {
+            grounded = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision) {
+        if (collision.gameObject.name == "Tilemap") {
+            grounded = false;
+        }
+    }
+}
