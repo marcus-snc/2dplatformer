30e0becbb780210210206f4d5a12da42f5e3dc06
daupaloffer <marcus@jadena.co.uk>
2020-02-03 22:42:52 +0000

    Wall-jump
    Add rudimentary walljump system and tweak other movement features and collision checks

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //public Rigidbody rb;
+    ///////////////////////////
+    // Variable declarations //
+    ///////////////////////////
+
     public Vector2 force;
     private Rigidbody2D rb;
-    public bool grounded = true;
+
+    // Ground check area points
+    public GameObject GgroundPointA, GgroundPointB;
+    public Vector2 groundPointA, groundPointB;
+
+    // Left wall check area points
+    public GameObject GleftWallPointA, GleftWallPointB;
+    public Vector2 leftWallPointA, leftWallPointB;
+
+    // Right wall check area points
+    public GameObject GrightWallPointA, GrightWallPointB;
+    public Vector2 rightWallPointA, rightWallPointB;
+
+    public Collider2D[] overlaps = new Collider2D[4];
+    public LayerMask layer;
+
+    public int grounded;
+    public int leftWallHang;
+    public int rightWallHang;
+
+    public bool justHung;
+
     // Start is called before the first frame update
     void Start() {
         force = new Vector2(1, 0);
@@ -16,27 +40,78 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
+        CollisionCheck(); // Call collision check function
 
+        if (Input.GetButtonDown("Jump")) { // Jump is here instead of FixedUpdate because GetButtonDown likes it better here? not sure
+            if (grounded > 0 || justHung == true || leftWallHang == 1 || rightWallHang == 1) {
+                rb.AddRelativeForce(Vector2.up * 1000);
+            }
+        }
+
+        /*if (leftWallHang == 1 || rightWallHang == 1) {
+            rb.gravityScale = 2;
+        }*/
+
+        // Check if player just came off wall hang
+        if (rb.gravityScale == 2 && Input.GetKey("d") == false && Input.GetKey("d") == false) {
+            rb.gravityScale = 10;
+            StartCoroutine(PostHangJumpTime());
+        }
+        // Check if player is midair, resetting previous antigravity if so
+        else if (leftWallHang == 0 && rightWallHang == 0 && grounded == 0) {
+            rb.gravityScale = 10;
+        }
+        else if (leftWallHang == 1 || rightWallHang == 1) {
+            StartCoroutine(PostHangJumpTime());
+        }
     }
     void FixedUpdate() {
+        //////////////
+        // Controls //
+        //////////////
+        
         if (Input.GetKey("d")) {
             //Debug.Log(Input.GetAxis("Horizontal"));
             //rb.AddForce(new Vector2(1,0)*10);
-            rb.AddRelativeForce(Vector2.right * 75);
+            if (rightWallHang == 0) {
+                rb.AddRelativeForce(Vector2.right * 75);
+            }
+            else if (rightWallHang == 1 && grounded == 0) {
+                rb.gravityScale = 2;
+            }
         }
         else if (Input.GetKey("a")) {
             //Debug.Log(Input.GetAxis("Horizontal"));
             //rb.AddForce(new Vector2(-1,0)*10);
-            rb.AddRelativeForce(Vector2.left * 75);
-        }
-
-        if (Input.GetButton("Jump")) {
-            if (grounded == true) {
-                rb.AddRelativeForce(Vector2.up * 500);
+            if (leftWallHang == 0) {
+                rb.AddRelativeForce(Vector2.left * 75);
+            }
+            else if (leftWallHang == 1 && grounded == 0) {
+                rb.gravityScale = 2;
             }
         }
     }
-    void OnCollisionEnter2D(Collision2D collision) {
+    void CollisionCheck() { // Translate GameObject point markers to Vector2, and calculate if overlapping with level
+        groundPointA = GgroundPointA.transform.position;
+        groundPointB = GgroundPointB.transform.position;
+
+        leftWallPointA = GleftWallPointA.transform.position;
+        leftWallPointB = GleftWallPointB.transform.position;
+
+        rightWallPointA = GrightWallPointA.transform.position;
+        rightWallPointB = GrightWallPointB.transform.position;
+
+        grounded = Physics2D.OverlapAreaNonAlloc(groundPointA, groundPointB, overlaps, layer);
+        leftWallHang = Physics2D.OverlapAreaNonAlloc(leftWallPointA, leftWallPointB, overlaps, layer);
+        rightWallHang = Physics2D.OverlapAreaNonAlloc(rightWallPointA, rightWallPointB, overlaps, layer);
+    }
+    IEnumerator PostHangJumpTime() {
+        justHung = true;
+        yield return new WaitForSeconds(0.25f);
+        justHung = false;
+    }
+    // OLD COLLISION CHECK METHOD
+    /*void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.name == "Tilemap") {
             grounded = true;
         }
@@ -45,5 +120,5 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.name == "Tilemap") {
             grounded = false;
         }
-    }
+    }*/
 }
